bdd5f355dbaebf55ef958a56dba16814410a8f1d
Christian Nassif-Haynes <DoxasticFox@bitbucket.org>
2015-12-16 17:00:25 +1100

    #2 should be done. Just needs some testing.

---
+++ postproc.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# This would be more robust (and 100 lines longer) if I used xpath voodoo.
+
+sed -i -e \
+'s/\(input \)\(ref="Band_Number"\)/\1faims_attribute_type="measure" \2 faims_attribute_name="Band Number"/g' \
+module/ui_schema.xml
