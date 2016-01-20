62e37801a6e79b861e61f13502cf77714d2c6a3d
Christian Nassif-Haynes <DoxasticFox@bitbucket.org>
2016-01-21 03:16:23 +1100

    Misc bugfixes which weren't pushed sooner for some reason...

--- postproc.sh
+++ postproc.sh
@@ -3,5 +3,5 @@
 # This would be more robust (and 100 lines longer) if I used xpath voodoo.
 
 sed -i -e \
-'s/\(input \)\(ref="Band_Number"\)/\1faims_attribute_type="measure" \2 faims_attribute_name="Band Number"/g' \
+'s/\(input \)\(ref="Band_Number_Measure"\)/\1faims_attribute_type="measure" \2 faims_attribute_name="Band Number"/g' \
 module/ui_schema.xml
