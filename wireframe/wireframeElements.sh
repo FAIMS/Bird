73d07c0d25b22141d4ddf510a854d46a52f798ec
Christian Nassif-Haynes <DoxasticFox@bitbucket.org>
2016-02-16 15:05:03 +1100

    Added autogenerated files

---
+++ wireframe/wireframeElements.sh
@@ -0,0 +1,91 @@
+
+rmdir -rf wireframeImages/
+mkdir -p wireframeImages
+
+
+  ./makeElement.sh style_orientation_orientation "horizontal" input 1 false false false false false wireframeImages/
+  
+  ./makeElement.sh style_even_layout_weight "1" input 1 false false false false false wireframeImages/
+  
+  ./makeElement.sh style_large_layout_weight "3" input 1 false false false false false wireframeImages/
+  
+  ./makeElement.sh User_User_Select_User "Select User" list 1 false false false false false wireframeImages/
+  
+  ./makeElement.sh Start_Main_Band_Number "" dropdown 1 false false false false false wireframeImages/
+  
+  ./makeElement.sh Start_Main_Bird_Species "" dropdown 1 false false false false false wireframeImages/
+  
+  ./makeElement.sh Start_Main_Location_Name "" dropdown 1 false false false false false wireframeImages/
+  
+  ./makeElement.sh Start_Main_Create_Bird "" button 1 false false false false false wireframeImages/
+  
+  ./makeElement.sh Start_Search_Search_Term "Search Term" input 2 false false false false false wireframeImages/
+  
+  ./makeElement.sh Start_Search_Search_Button "Search" button 2 false false false false false wireframeImages/
+  
+  ./makeElement.sh Start_Search_Entity_List "Entity List" list 1 false false false false false wireframeImages/
+  
+  ./makeElement.sh Start_Map_map "" map 1 false false false false false wireframeImages/
+  
+  ./makeElement.sh Bird_Bird_Info_Band_Number "" dropdown 1 false false true true false wireframeImages/
+  
+  ./makeElement.sh Bird_Bird_Info_Band_Number_Measure "" input 1 false false false false false wireframeImages/
+  
+  ./makeElement.sh Bird_Bird_Info_Bird_Species "" dropdown 1 false false true false false wireframeImages/
+  
+  ./makeElement.sh Bird_Bird_Info_Location_Name "" dropdown 1 false false true false false wireframeImages/
+  
+  ./makeElement.sh Bird_Bird_Info_Latitude "Latitude" input 2 false false true false true wireframeImages/
+  
+  ./makeElement.sh Bird_Bird_Info_Northing "Northing" input 2 false false true false true wireframeImages/
+  
+  ./makeElement.sh Bird_Bird_Info_Longitude "Longitude" input 2 false false true false true wireframeImages/
+  
+  ./makeElement.sh Bird_Bird_Info_Easting "Easting" input 2 false false true false true wireframeImages/
+  
+  ./makeElement.sh Bird_Bird_Info_Take_From_GPS "Take From GPS" button 1 false false false false false wireframeImages/
+  
+  ./makeElement.sh Bird_Bird_Info_Nest_ID "" input 1 false false true false false wireframeImages/
+  
+  ./makeElement.sh Bird_Bird_Info_Chick_ID "" input 1 false false true false false wireframeImages/
+  
+  ./makeElement.sh Bird_Bird_Info_Age "" radio 1 false false true true false wireframeImages/
+  
+  ./makeElement.sh Bird_Bird_Info_Sex "" radio 1 false false true true false wireframeImages/
+  
+  ./makeElement.sh Bird_Bird_Info_How_Caught "" radio 1 false false true false false wireframeImages/
+  
+  ./makeElement.sh Bird_Bird_Info_Capture_Status "" checkbox 1 false false true false false wireframeImages/
+  
+  ./makeElement.sh Bird_Bird_Info_Bill_Colour "" pictureGallery 1 false false true false false wireframeImages/
+  
+  ./makeElement.sh Bird_Bird_Info_Timestamp "" input 1 false false false false true wireframeImages/
+  
+  ./makeElement.sh Bird_Bird_Info_Author "" input 1 false false false false true wireframeImages/
+  
+  ./makeElement.sh Bird_Measurements_Tarsus_Length "" input 1 false false true false false wireframeImages/
+  
+  ./makeElement.sh Bird_Measurements_Head_Length "" input 1 false false true false false wireframeImages/
+  
+  ./makeElement.sh Bird_Measurements_Bill_Length "" input 2 false false true false false wireframeImages/
+  
+  ./makeElement.sh Bird_Measurements_Bill_Height "" input 2 false false true false false wireframeImages/
+  
+  ./makeElement.sh Bird_Measurements_Wing_Length "" input 1 false false true false false wireframeImages/
+  
+  ./makeElement.sh Bird_Measurements_Tail_Length "" input 1 false false true false false wireframeImages/
+  
+  ./makeElement.sh Bird_Measurements_Mass "" input 1 false false true false false wireframeImages/
+  
+  ./makeElement.sh Bird_Comments_ID_Pit_Tag "" input 1 false false true true false wireframeImages/
+  
+  ./makeElement.sh Bird_Comments_Colour_Band "" pictureGallery 1 false false true true false wireframeImages/
+  
+  ./makeElement.sh Bird_Comments_Comments "" input 1 false false true true false wireframeImages/
+  
+  ./makeElement.sh Bird_Comments_Sample "" checkbox 1 false false true true false wireframeImages/
+  
+  ./makeElement.sh Bird_Comments_Attach_Photograph "" camera 1 false false true false false wireframeImages/
+  
+  ./makeElement.sh Bird_Comments_Button_Attach_Photograph "" button 1 false false false false false wireframeImages/
+  
